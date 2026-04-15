a2a9a01d2f837fc02d79effe12096fd9e40c5d1a
zzungju <zzungju07@gmail.com>
2026-04-15 16:57:30 +0900

    :sparkles: Feat: implement interactive map using Leaflet and OpenStreetMap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import EditListIcon from '@/assets/icons/List_Unordered.svg?react';
 import EditCardListIcon from '@/assets/icons/Cards.svg?react';
 
 import DestinationSection from '@/components/destinationsSection';
+import RouteMap from '@/components/routeMap';
 
 export default function Home() {
 
@@ -19,14 +20,16 @@ export default function Home() {
   return (
     <div>
       <main className="h-screen flex flex-1 flex-col gap-5 px-4 pb-28 pt-2">
-      <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1">
           <h1 className="text-24 text-800 text-gray-900">프랑스 길</h1>
           <ArrowCaretDownIcon className="size-6 shrink-0" aria-hidden />
         </div>
 
         <section aria-label="전체 지도">
           <h2 className="text-20 text-600 text-gray-900">전체 지도</h2>
-          <div className="relative mt-2 h-[183px] overflow-hidden rounded-xl bg-primary-50" />
+          <div className="relative mt-2 h-[183px] overflow-hidden rounded-xl bg-primary-50">
+            <RouteMap />
+          </div>
         </section>
 
         <section aria-label="주요 목적지">
